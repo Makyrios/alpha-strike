c5effbc78bb6c2932c99c6aaa6deeef4d4646d3b
Makyrios <lavreniuk.mykyta@lll.kpi.ua>
2023-11-29 20:10:44 +0200

    Added start game widget timer

---
+++ Source/AlphaStrike/Private/UI/Widgets/AS_StartGameWidget.cpp
@@ -0,0 +1,40 @@
+﻿// AlphaStrike by Team #1. AlphaNova courses🤙
+
+
+#include "UI/Widgets/AS_StartGameWidget.h"
+#include "Components/TextBlock.h"
+
+
+void UAS_StartGameWidget::InitializeWidget(float DelayStartTimer)
+{
+    StartGameDelay = DelayStartTimer;
+    Timer = StartGameDelay;
+    bStartTimer = true;
+}
+
+
+void UAS_StartGameWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+    Super::NativeTick(MyGeometry, InDeltaTime);
+
+    if (bStartTimer)
+    {
+        Timer -= InDeltaTime;
+        if (StartGameDelay - Timer < 1)
+        {
+            DisplayText->SetText(FText::FromString("Get Ready!"));
+        }
+        else if (FMath::CeilToInt(Timer) == 0)
+        {
+            DisplayText->SetText(FText::FromString("Go!"));
+        }
+        else if (Timer > 0)
+        {
+            DisplayText->SetText(FText::AsNumber(FMath::CeilToInt(Timer)));
+        }
+        else
+        {
+            RemoveFromParent();
+        }
+    }
+}
---
+++ Source/AlphaStrike/Public/UI/Widgets/AS_StartGameWidget.h
@@ -0,0 +1,34 @@
+﻿// AlphaStrike by Team #1. AlphaNova courses🤙
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AS_StartGameWidget.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class ALPHASTRIKE_API UAS_StartGameWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+    class UCanvasPanel* Canvas;
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+    class UTextBlock* DisplayText;
+
+public:
+    void InitializeWidget(float DelayStartTimer);
+
+private:
+    float StartGameDelay;
+    float Timer;
+
+    bool bStartTimer = false;
+
+    virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+};
